da99ba8940b40ebf1b0a23b7e9104bbfe80389d5
Takuhei Mizutani <takuheim1011@gmail.com>
2021-04-21 13:29:34 +0900

    list sortable logic working

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,6 +1,7 @@
 class Playlist < ApplicationRecord
-  has_many :playlist_items
+  has_many :playlist_items, -> { order(position: :asc) }
   has_many :songs, through: :playlist_items
+
 
   belongs_to :user
   validates :name, presence: true, uniqueness: true
--- app/models/playlist_item.rb
+++ app/models/playlist_item.rb
@@ -2,4 +2,5 @@ class PlaylistItem < ApplicationRecord
   belongs_to :playlist
   belongs_to :song
   validates :song, :playlist, presence: true
+  acts_as_list scope: :playlist
 end
